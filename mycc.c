0a2173b174a1e951097dd0f5632e73c8f535c5a9
kura <kura19971106@gmail.com>
2018-11-09 17:12:14 +0900

    add mul/div

--- mycc.c
+++ mycc.c
@@ -3,16 +3,21 @@
 
 #define NUM_TK 1000
 
+void read_add_sub();
+void read_mul_div();
+
 struct Token{
     int kind;
     int value;
 };
 
 enum token_kind{
-    TK_EOF = 0,  
-    TK_INT = 1,  
-    TK_PLUS = 2,  
-    TK_MINUS = 3  
+    TK_INT = 0,  
+    TK_PLUS = 1,  
+    TK_MINUS = 2,
+    TK_MUL = 3, 
+    TK_DIV = 4,  
+    TK_EOF = 5  
 };
 
 struct Token tokens[NUM_TK];
@@ -45,6 +50,12 @@ void tokenize(){
             else if(ch == '-'){
                 tokens[idx++].kind = TK_MINUS;
             }
+            else if(ch == '*'){
+                tokens[idx++].kind = TK_MUL;
+            }
+            else if(ch == '/'){
+                tokens[idx++].kind = TK_DIV;
+            }
         } 
     }
     if(num_values > 0){
@@ -55,6 +66,12 @@ void tokenize(){
     }
     tokens[idx].kind = TK_EOF;
     num_tokens += idx;
+}
+
+struct Token* read_token(){
+    struct Token* token;
+    token = &tokens[token_idx];
+    return token;
 }
 
 struct Token* get_token(){
@@ -69,37 +86,61 @@ int end_tokens(){
     else return 0;
 }
 
+void read_add_sub(){
+    read_mul_div(); 
+
+    while(!end_tokens()){
+        printf("  pushq %%rax\n");
+        struct Token* tk = get_token();
+        read_mul_div();
+        printf("  pop %%rbx\n");
+        if(tk->kind == TK_PLUS){
+            printf("  addl %%ebx, %%eax\n");
+        }
+        else if(tk->kind == TK_MINUS){
+            printf("  subl %%eax, %%ebx\n");
+            printf("  movl %%ebx, %%eax\n");
+        }
+        else{
+            printf("Error(read_add_sub). read token : %d.", tk->kind);
+        }
+    }
+}
+
+void read_mul_div(){
+    struct Token* tk1 = get_token();
+    if(tk1->kind != TK_INT){
+        printf("Error(read_mul_div). read token : %d\n", tk1->kind);
+    }
+    printf("  movl $%d, %%eax\n", tk1->value);
+
+    while(read_token()->kind == TK_MUL || read_token()->kind == TK_DIV){
+        struct Token* tk2 = get_token();
+        struct Token* tk3 = get_token();
+
+        if(tk2->kind == TK_MUL){
+            printf("  movl $%d, %%ebx\n", tk3->value);
+            printf("  imul %%ebx, %%eax\n");
+        }
+        else if(tk2->kind == TK_DIV){
+            printf("  movl $%d, %%ebx\n", tk3->value);
+            printf("  cltd\n");
+            printf("  div %%ebx\n");
+        }
+        else{
+            printf("Error(read_mul_div). read token : %d.", tk2->kind);
+        }
+    }
+}
+
 int main(){
     tokenize();
     printf(".global main\n");
     printf("main:\n");
 
-    struct Token *tk = get_token();
-    if(tk->kind != TK_INT){
-        printf("Error. need INT token.\n");
-    }
-    printf("  movl $%d, %%eax\n", tk->value);
+    read_add_sub();
 
-    while(!end_tokens()){
-        struct Token *tk2 = get_token();
-        struct Token *tk3 = get_token();
-
-        if(tk3->kind != TK_INT){
-            printf("Error. need INT token.\n");
-            return -1;
-        }
-
-        if(tk2->kind == TK_PLUS){
-            printf("  addl $%d, %%eax\n", tk3->value);
-        }
-        else if(tk2->kind == TK_MINUS){
-            printf("  subl $%d, %%eax\n", tk3->value);
-        }
-        else{
-            printf("Error. need PLUS/MINUS token.\n");
-            return -1;
-        }
-    }
     printf("  ret\n");
+
     return 0;
 }
--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 runtest() {
     echo "$1" | ./mycc > tmp.s
 
-    cc -o tmp tmp.s
+    gcc -o tmp tmp.s
     ./tmp
 
     out=$?
@@ -12,11 +12,15 @@ runtest() {
         exit 1
     fi
     echo "$1 => $2"
-    rm tmp tmp.s
+    rm tmp 
 }
 
 runtest "3+2" 5
-runtest "254+1" 255
-runtest "200-30" 170
+runtest "3+4+1" 8
+runtest "80-30" 50
 runtest "8-5+3" 6
 runtest "100-40+20-10" 70
+runtest "6*4" 24
+runtest "4/2" 2
+runtest "6*4/2" 12
+runtest "5+2*4-8" 5
